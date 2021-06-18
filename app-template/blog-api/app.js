0c7339e9a8d32d0b7f902fa7173ca8234b2e21c3
Una Emelia <unaemelia@gmail.com>
2021-06-18 07:26:39 +0200

    blog-api changes

--- app-template/blog-api/app.js
+++ app-template/blog-api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');     // Import express, a lightweight framework
 const app = express();                  // Init express and saving it in "app" variable
 const mongoose = require('mongoose');  // Import mongoose, used for handling DB and giving a NoSQL DB, such as MongoDB, the abilities of a relational DB, such as MySQL
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -10,39 +11,26 @@ app.use(cors());              // Allow cross-origin requests
 
 
 // ROUTES
-const postsRoute = require('./routes/posts');
-app.use('/posts', postsRoute);
+const productsRoute = require('./routes/products');
+app.use('/products', productsRoute);
 
 
 app.get('/', (req, res) => {
     res.send('Home page')
 });
 
-// const Post = require('./models/Post');
-// app.get('/posts', async (req, res) => {
-//     // res.send('a list of all posts');
-//     const posts = await Post.find();
-//     res.send(posts);
-// });
-
-
-// app.get('/contact', (req, res) => {
-//     res.send('Contact page')
-// });
-
-
-
 
 // DB connections
 // mongodb+srv://<username>:<password>@cluster0.wffly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 mongoose.connect(
-    'mongodb+srv://blog_user:123qwe123@cluster0.wffly.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    'mongodb+srv://shop_user:12345@cluster0.qmnme.mongodb.net/Cluster0?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true },
-    () => { 
+    () => {
         console.log('DB connected')
     }
 );
 
 
-// Start listening to the server
+// Start listening to the server.
 app.listen(5000) // the to requests through port 5000
---
+++ app-template/blog-api/models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('products', ProductSchema);
