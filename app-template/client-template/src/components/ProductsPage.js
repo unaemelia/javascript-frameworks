c9923a226037df76fbc9a3221cbc3f4983a13a54
Muhammad Nauman <naumanmalik775@gmail.com>
2021-06-24 18:32:29 +0200

    small updations

--- app-template/client-template/src/components/ProductsPage.js
+++ app-template/client-template/src/components/ProductsPage.js
@@ -1,21 +1,27 @@
 import React from "react";
 import proimage from "../images/pro-image.jpg";
 import { Link } from "react-router-dom";
-import { Button } from '.././styles/Styling';
+import { Button } from ".././styles/Styling";
 
 const ProductsPage = ({ product }) => {
-    return (
-        <div className="product-item" key={product['_id']}>
-            <h2>{product.title}</h2>
-            <img
-                className="img-thumbnail"
-                src={product.img ? product.img : proimage}
-                alt={product.title}
-            />
-            <p>Price: {product.price} SEK</p>
-            <Link to={`/item/${product['_id']}`}><Button>View Product</Button></Link>
-        </div>
-    );
+  return (
+    <div className="product-item" key={product["_id"]}>
+      <h2>{product.title}</h2>
+      <img
+        className="img-thumbnail"
+        src={product.img ? product.img : proimage}
+        alt={product.title}
+      />
+      <p>
+        Price: <span className="price_label"> {product.price} SEK</span>
+      </p>
+      <p className="category_label">{product.category}</p>
+      <p className="out_of_stock">{product.stock < 1 ? "Out of Stock" : ""}</p>
+      <Link to={`/item/${product["_id"]}`}>
+        <Button>View Product</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default ProductsPage;
