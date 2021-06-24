d617fa603156cd8513a590faacc18a9153919a74
Muhammad Nauman <naumanmalik775@gmail.com>
2021-06-24 15:17:52 +0200

    manage products style update

--- app-template/client-template/src/components/Product.js
+++ app-template/client-template/src/components/Product.js
@@ -1,34 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "../styles/manageProducts.module.css";
 
 function Product({ product, deleteProduct }) {
-    const formatDate = (date) => {
-        let dateObj = new Date(date);
+  const formatDate = (date) => {
+    let dateObj = new Date(date);
 
-        return `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()} ${dateObj.getHours()}:${dateObj.getMinutes()}`;
-    }
+    return `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()} ${dateObj.getHours()}:${dateObj.getMinutes()}`;
+  };
 
-    const handleDeleteProduct = () => {
-        deleteProduct(product['_id']);
-    }
+  const handleDeleteProduct = () => {
+    deleteProduct(product["_id"]);
+  };
 
-
-    return (
-        <tbody>
-            <tr>
-                <td>{product.title}</td>
-                <td>{product.category}</td>
-                <td>{product.description}</td>
-                <td>{product.price}</td>
-                <td>{product.stock}</td>
-                <td>{formatDate(product.date)}</td>
-                <td>
-                    <Link to={`/update-item/${product['_id']}`}><button>Update</button></Link>
-                    <button onClick={handleDeleteProduct}>Delete</button>
-                </td>
-            </tr>
-        </tbody>
-    )
+  return (
+    <>
+      <tr className={style.table_row}>
+        <td>{product.title}</td>
+        <td>{product.category}</td>
+        <td>{product.description}</td>
+        <td>{product.price}</td>
+        <td>{product.stock}</td>
+        <td>{formatDate(product.date)}</td>
+        <td>
+          <Link to={`/update-item/${product["_id"]}`}>
+            <button className="btn update_btn">Update</button>
+          </Link>
+          <button className="del_btn btn" onClick={handleDeleteProduct}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    </>
+  );
 }
 
-export default Product
+export default Product;
--- app-template/client-template/src/components/ProductList.js
+++ app-template/client-template/src/components/ProductList.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import Product from './Product';
+import React from "react";
+import Product from "./Product";
+import style from "../styles/manageProducts.module.css";
 
 function ProductList({ products, deleteProduct }) {
-
-    return (
-        <table>
-            < thead >
-                <tr>
-                    <th>Title</th>
-                    <th>Category</th>
-                    <th className="product-description">Description</th>
-                    <th>Price</th>
-                    <th>Stock</th>
-                    <th>Date</th>
-                    <th>Action</th>
-                </tr>
-            </thead >
-            {
-                products.map((product) => (
-                    <Product
-                        key={product['_id']}
-                        product={product}
-                        deleteProduct={deleteProduct}
-                    />
-                ))
-            }
-        </table>
-    )
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Category</th>
+          <th className={style.product_description}>Description</th>
+          <th>Price</th>
+          <th>Stock</th>
+          <th>Date</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <Product
+            key={product["_id"]}
+            product={product}
+            deleteProduct={deleteProduct}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default ProductList
+export default ProductList;
