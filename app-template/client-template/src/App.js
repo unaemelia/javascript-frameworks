047bf05c942873b274ae895e529738d6515606e4
Una Emelia <unaemelia@gmail.com>
2021-06-24 10:34:08 +0200

    testing

--- app-template/client-template/src/App.js
+++ app-template/client-template/src/App.js
@@ -12,7 +12,7 @@ import UpdateItem from "./pages/admin/UpdateItem";
 function App() {
   return (
     <div className="App">
-      <div>testing again</div>
+      <div>testing again and again</div>
       <Router>
         <Nav />
         <Switch>
