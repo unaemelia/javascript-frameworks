5cb564c1cbe09fd114bb6d12f6a7387337c53946
Muhammad Nauman <naumanmalik775@gmail.com>
2021-06-24 16:27:51 +0200

    commit

--- app-template/client-template/src/components/ProductForm.js
+++ app-template/client-template/src/components/ProductForm.js
@@ -1,124 +1,124 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import styles from '../styles/Forms.module.css'
+import styles from "../styles/Forms.module.css";
 
 const ProductForm = ({
-    title,
-    setTitle,
-    description,
-    setDescription,
-    price,
-    setPrice,
-    stock,
-    setStock,
-    category,
-    setCategory,
-    img,
-    setImg,
-    handleSubmit,
-    formId,
+  title,
+  setTitle,
+  description,
+  setDescription,
+  price,
+  setPrice,
+  stock,
+  setStock,
+  category,
+  setCategory,
+  img,
+  setImg,
+  handleSubmit,
+  formId,
 }) => {
-    const formDataHandler = (e) => {
-        e.preventDefault();
-        const formData = {
-            title: title,
-            description: description,
-            price: price,
-            stock: stock,
-            category: category,
-            img: img,
-        };
-        handleSubmit(formData);
+  const formDataHandler = (e) => {
+    e.preventDefault();
+    const formData = {
+      title: title,
+      description: description,
+      price: price,
+      stock: stock,
+      category: category,
+      img: img,
     };
-    //////////Fetch Categories///////
-    const [categories, setCategories] = useState([]);
-    const fetchCategories = async () => {
-        const res = await fetch("http://localhost:5000/categories");
-        const cat = await res.json();
-        setCategories(cat);
-    };
+    handleSubmit(formData);
+  };
+  //////////Fetch Categories///////
+  const [categories, setCategories] = useState([]);
+  const fetchCategories = async () => {
+    const res = await fetch("http://localhost:5000/categories");
+    const cat = await res.json();
+    setCategories(cat);
+  };
 
-    useEffect(() => {
-        fetchCategories();
-    }, []);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-    return (
-        <div className={styles.container}>
-            <form onSubmit={formDataHandler}>
-                <div className="form-row">
-                    <input
-                        className={styles.input}
-                        placeholder="Your Title * "
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </div>
-                <div className="form-row">
-                    <textarea
-                        cols="30"
-                        rows="10"
-                        className={styles.textarea}
-                        placeholder="Your Description *"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </div>
-
-                <div className="form-row">
-                    <input
-                        className={styles.input}
-                        placeholder="Your Price *"
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)}
-                    />
-                </div>
-                <div className="form-row">
-                    <input
-                        className={styles.input}
-                        placeholder="Your Stock * "
-                        value={stock}
-                        onChange={(e) => setStock(e.target.value)}
-                    />
-                </div>
-                <div className="form-row">
-                    <input
-                        className={styles.input}
-                        placeholder="Image URL * "
-                        type="text"
-                        value={img}
-                        onChange={(e) => setImg(e.target.value)}
-                    />
-                </div>
-
-                <div className={styles.form1}>
-                    <select
-                        className={styles.selectCategory}
-                        value={category}
-                        onChange={(e) => setCategory(e.target.value)}
-                    >
-                        <option value="">Select Category</option>
-                        {categories.map((category) => (
-                            <option key={category._id} value={category.title}>
-                                {category.title}
-                            </option>
-                        ))}
-                    </select>
-                    <Link to="/create-category">
-                        <button className={styles.categorybtn}>
-                            Create a new category
-                        </button>
-                    </Link>
-                </div>
-
-                <button type="submit" className={styles.button}>
-                    {formId === "create-item" ? "Create" : "Update"}
-                </button>
-                <Link to="/manage-items">
-                    <button className={styles.button}> Back</button>
-                </Link>
-            </form>
+  return (
+    <div className={styles.container}>
+      <form onSubmit={formDataHandler}>
+        <div className="form-row">
+          <input
+            className={styles.input}
+            placeholder="Product Title * "
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-    );
+        <div className="form-row">
+          <textarea
+            cols="30"
+            rows="10"
+            className={styles.textarea}
+            placeholder="Product Description *"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+
+        <div className="form-row">
+          <input
+            className={styles.input}
+            placeholder="Product Price *"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <div className="form-row">
+          <input
+            className={styles.input}
+            placeholder="Product Stock * "
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+          />
+        </div>
+        <div className="form-row">
+          <input
+            className={styles.input}
+            placeholder="Image URL  "
+            type="text"
+            value={img}
+            onChange={(e) => setImg(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.form1}>
+          <select
+            className={styles.selectCategory}
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">Select Category</option>
+            {categories.map((category) => (
+              <option key={category._id} value={category.title}>
+                {category.title}
+              </option>
+            ))}
+          </select>
+          <Link to="/create-category">
+            <button className={styles.categorybtn}>
+              Create a new category
+            </button>
+          </Link>
+        </div>
+
+        <button type="submit" className={styles.button}>
+          {formId === "create-item" ? "Create" : "Update"}
+        </button>
+        <Link to="/manage-items">
+          <button className={styles.button}> Back</button>
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default ProductForm;
--- app-template/client-template/src/pages/admin/ManageItems.js
+++ app-template/client-template/src/pages/admin/ManageItems.js
@@ -1,53 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import ProductList from '../../components/ProductList';
-import { Link } from 'react-router-dom';
-import { Title, PageWrapper } from '../../styles/Styling';
+import React, { useState, useEffect } from "react";
+import ProductList from "../../components/ProductList";
+import { Link } from "react-router-dom";
+import { Title, PageWrapper } from "../../styles/Styling";
+import styles from "../../styles/Forms.module.css";
 
 function ManageItems() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    // get all products
-    const fetchProducts = async () => {
-        try {
-            const response = await fetch('http://localhost:5000/products');
-            if (!response.ok) {
-                throw new Error('Server error ' + response.status);
-            }
-            const data = await response.json();
+  // get all products
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/products");
+      if (!response.ok) {
+        throw new Error("Server error " + response.status);
+      }
+      const data = await response.json();
 
-            setProducts(data);
-        } catch (error) {
-            console.log(error);
-        }
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    //delete product
-    const deleteProduct = async (productId) => {
-        try {
-            await fetch(`http://localhost:5000/products/${productId}`, {
-                method: 'DELETE',
-            });
+  //delete product
+  const deleteProduct = async (productId) => {
+    try {
+      await fetch(`http://localhost:5000/products/${productId}`, {
+        method: "DELETE",
+      });
 
-            fetchProducts();
-        } catch (error) {
-            console.log(error);
-        }
+      fetchProducts();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <PageWrapper>
-            <Title>Manage Products</Title>
-            <Link to="/create-item"><button>Create Items</button></Link>
+  return (
+    <PageWrapper>
+      <Title>Manage Products</Title>
+      <Link to="/create-item">
+        <button className={styles.button}>Add new Product</button>
+      </Link>
 
-            <section id="displayProducts">
-                <ProductList products={products} deleteProduct={deleteProduct} />
-            </section>
-        </PageWrapper>
-    )
+      <section id="displayProducts">
+        <ProductList products={products} deleteProduct={deleteProduct} />
+      </section>
+    </PageWrapper>
+  );
 }
 
-export default ManageItems
+export default ManageItems;
